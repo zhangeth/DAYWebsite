9d6dcfda93dfa7b38fef8c6a724bf3f89f07f32d
Ethan Zhang <zhangeth@usc.edu>
2022-11-22 17:23:00 -0800

    added header, changed style sheet

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+
+
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
+"<p>&copy;  " + new Date().getFullYear() + " Ethan Zhang. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>Data</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='art.html'>Art</a></li>" +
+"</ul>";
